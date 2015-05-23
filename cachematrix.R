952fa382195b88b129688827fff2042b685c5e0d
James Clawson <clawson@gmail.com>
2015-05-22 19:46:55 -0500

    added two functions for assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Together, these two functions will allow me to define a matrix and
+## quickly call up its inverse, calling on a cache if one exists to
+## save some time. Apparently my matrices are big and my time is
+## very, very valuable.
 
-## Write a short comment describing this function
+## makeCacheMatrix will hold a defined matrix and a given cache
+## that is passed to it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL ## initializes m, which will serve as the cache
+  set <- function(y) { ## function will set the matrix
+    x <<- y ## defines x of a parent function
+    m <<- NULL ## whenever we set the matrix, we need to reset the cache
+  }
+  get <- function() { ## function will return the matrix
+    x
+  }
+  savecache <- function(cache) { ## function will save the cache to m
+    m <<- cache
+  }
+  getcache <- function() { ## function will show the cache
+    m
+  }
+  list(set = set, get = get,
+       savecache = savecache,
+       getcache = getcache) ## names a list of functions
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will check to see if a given matrix's inverse has been
+## cached already. If it has, it will return the cache. Otherwise,
+## it will invert the matrix, save the inversion to a cache, and 
+## then print the inverted matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getcache() ## assigns m from makeCacheMatrix's cache for 'x'
+  if(!is.null(m)) { ## reacts if the cache is defined
+    message("getting cached data") ## signals that data is cached
+    return(m) ## prints cache
+  }
+  data <- x$get() ## pulls the matrix 'x', assigns to 'data'
+  m <- solve(data, ...) ## inverts matrix and assigns it to amount 'm'
+  x$savecache(m) ## saves 'm' to the cache
+  m ## prints this amount 'm'
 }
